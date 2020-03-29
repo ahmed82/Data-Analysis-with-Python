b9d712b5ce6e5ff8c3f519cd6f1dc4cff335716c
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-03-28 21:16:14 -0400

    start model 3

---
+++ lab_files/03-EXPLORATORY DATA ANALYSIS.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 20:59:28 2020
+
+@author: 1426391
+
+EXPLORATORY DATA ANALYSIS
+"""
+
+# 1. Import Data from Module 2
+import pandas as pd
+import numpy as np
+
+# Load data and store in dataset
+path='https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/automobileEDA.csv'
+df = pd.read_csv(path)
+df.head()
+
+"""
+2. Analyzing Individual Feature Patterns using Visualization
+To install seaborn we use the pip which is the python package manager.
+"""
+## %%capture
+#! pip install seaborn
+
+#Import visualization packages "Matplotlib" and "Seaborn", don't forget about "%matplotlib inline" 
+#to plot in a Jupyter notebook.
+import matplotlib.pyplot as plt
+import seaborn as sns
+#%matplotlib inline 
+#When visualizing individual variables, it is important to first understand what type of variable you are dealing with. This will help us find the right visualization method for that variable.
+
+# list the data types for each column
+print(df.dtypes)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

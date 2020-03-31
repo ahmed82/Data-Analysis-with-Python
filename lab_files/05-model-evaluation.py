9d1a8e58fbfda2e412b5275eb3b0d2543a079eb2
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-03-31 16:30:31 -0400

    start module 5

---
+++ lab_files/05-model-evaluation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 13:41:27 2020
+
+@author: 1426391
+"""
+
+
+
+
+"""What is the correct use of the "train_test_split" function such that 40% 
+of the data samples will be utilized for testing, the parameter "random_state" 
+is set to zero, and the input variables for the features and targets 
+are_data, y_data respectively.
+
+Answer:
+     train_test_split(x_data, y_data, test_size=0.4, random_state=0) - correct
+    """
